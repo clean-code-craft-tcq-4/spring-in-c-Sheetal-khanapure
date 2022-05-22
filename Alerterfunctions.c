a7ac19af7bd7848abdab7b201564dc58c94c724a
Sheetal-khanapure <105204580+Sheetal-khanapure@users.noreply.github.com>
2022-05-23 00:21:05 +0530

    Create Alerterfunctions.c

---
+++ Alerterfunctions.c
@@ -0,0 +1,23 @@
+#include "stats.h"
+void emailAlerter()
+{
+  emailAlertCallCount = 1;
+}
+
+void ledAlerter()
+{
+  ledAlertCallCount = 1;
+}
+
+alerter_funcptr alerters[0] = &emailAlerter;
+alerter_funcptr alerters[1] = &ledAlerter;
+
+void check_and_alert(maxThreshold,alerters,computedStats);
+{
+    if(computedStats.max>maxThreshold)
+    {
+      alerters[0];
+      alerters[1];
+    }
+}
+
